85fc486276a261646600fd484a3dea0dcac7372d
Chadwick Boulay <chadwick.boulay@gmail.com>
2012-03-22 16:48:59 +0900

    Renamed. Was confusing.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 dependencies = {
         'pyserial': '>=2.5'
 }
-setup(name='MagstimInterface',
+setup(name='Magstim',
       version='1.0',
-      packages = ['MagstimInterface'],
+      packages = ['Magstim'],
       requires = [('%s (%s)' % (p,v)).replace(' ()','') for p,v in dependencies.items()],  # available in distutils from Python 2.5 onwards
       install_requires = ['%s%s' % (p,v) for p,v in dependencies.items()],
 )

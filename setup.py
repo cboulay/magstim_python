9fa8eb8bcaab3f4ac3c1088c1ca67b407ee3bf19
Chadwick Boulay <chadwick.boulay@gmail.com>
2012-03-22 16:39:53 +0900

    better setup script

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
+dependencies = {
+        'pyserial': '>=2.5'
+}
 setup(name='MagstimInterface',
       version='1.0',
-      py_modules=['MagstimInterface'],
-      )
+      packages = ['MagstimInterface'],
+      requires = [('%s (%s)' % (p,v)).replace(' ()','') for p,v in dependencies.items()],  # available in distutils from Python 2.5 onwards
+      install_requires = ['%s%s' % (p,v) for p,v in dependencies.items()],
+)
